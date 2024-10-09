e83e5d080e792a93a03f516306547ca4ea918a9c
zphtet <zinphtet.215108@gmail.com>
2024-10-09 20:48:45 +0630

    ..

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-crud-postgres
 
+// dsn := "host=pg-17b26418-zinpainghtet-24dc.i.aivencloud.com user=avnadmin password=AVNS_AzpKrCNsRXGopmeiyYp dbname=defaultdb port=21847 sslmode=require"
+// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
--- initializer/connectDB.go
+++ initializer/connectDB.go
@@ -10,14 +10,13 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	var err error
-	// dsn := "host=pg-17b26418-zinpainghtet-24dc.i.aivencloud.com user=avnadmin password=AVNS_AzpKrCNsRXGopmeiyYp dbname=defaultdb port=21847  sslmode=require"
-	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// var err error
+	// db connection on read me
 
-	if err != nil {
-		fmt.Println("Error connection to database")
-		return
-	}
+	// if err != nil {
+	// 	fmt.Println("Error connection to database")
+	// 	return
+	// }
 	fmt.Println("DB connection established ", DB)
 
 }
