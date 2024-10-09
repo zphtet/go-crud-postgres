ca25ca881a11f9d32e3b3f5aecbfaec9893f3639
zphtet <zinphtet.215108@gmail.com>
2024-10-09 20:46:56 +0630

    comment

--- initializer/connectDB.go
+++ initializer/connectDB.go
@@ -4,7 +4,6 @@ import (
 	// "blog-crud/models"
 	"fmt"
 
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -12,8 +11,8 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	dsn := "host=pg-17b26418-zinpainghtet-24dc.i.aivencloud.com user=avnadmin password=AVNS_AzpKrCNsRXGopmeiyYp dbname=defaultdb port=21847  sslmode=require"
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// dsn := "host=pg-17b26418-zinpainghtet-24dc.i.aivencloud.com user=avnadmin password=AVNS_AzpKrCNsRXGopmeiyYp dbname=defaultdb port=21847  sslmode=require"
+	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		fmt.Println("Error connection to database")
